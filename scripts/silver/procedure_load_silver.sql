42e89ca76ee0f016490e4d9f5aa50273c31d0ff4
Mahalaxmi Somisetty <mahalaxmi1246@gmail.com>
2026-02-16 21:12:07 +0530

    Update procedure_load_silver.sql

--- scripts/silver/procedure_load_silver.sql
+++ scripts/silver/procedure_load_silver.sql
@@ -87,44 +87,69 @@ BEGIN
 
 
         -- =====================================================
-        -- Load: silver.crm_prd_info
-        -- =====================================================
-        SET @start_time = GETDATE();
+-- Load: silver.crm_prd_info (FINAL CORRECT VERSION)
+-- =====================================================
+SET @start_time = GETDATE();
 
-        PRINT '>> Truncating Table: silver.crm_prd_info';
-        TRUNCATE TABLE silver.crm_prd_info;
+PRINT '>> Truncating Table: silver.crm_prd_info';
+TRUNCATE TABLE silver.crm_prd_info;
 
-        PRINT '>> Inserting Data Into: silver.crm_prd_info';
-        INSERT INTO silver.crm_prd_info (
-            prd_id,
-            cat_id,
-            prd_key,
-            prd_nm,
-            prd_cost,
-            prd_line,
-            prd_start_dt,
-            prd_end_dt
-        )
-        SELECT
-            prd_id,
-            REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
-            SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
-            prd_nm,
-            ISNULL(prd_cost, 0) AS prd_cost,
-            CASE 
-                WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
-                WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
-                WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
-                WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
-                ELSE 'n/a'
-            END AS prd_line,
-            CAST(prd_start_dt AS DATE) AS prd_start_dt,
-            CAST(prd_end_dt AS DATE) AS prd_end_dt
-        FROM bronze.crm_prd_info;
+PRINT '>> Inserting Data Into: silver.crm_prd_info';
 
-        SET @end_time = GETDATE();
-        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
+WITH prd AS (
+    SELECT
+        prd_id,
+        REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+        SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key_clean,
+        TRIM(prd_nm) AS prd_nm,
+        ISNULL(prd_cost, 0) AS prd_cost,
+        CASE 
+            WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+            WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+            WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+            WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+            ELSE 'n/a'
+        END AS prd_line,
+        CAST(prd_start_dt AS DATE) AS prd_start_dt
+    FROM bronze.crm_prd_info
+),
+prd_versions AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY prd_key_clean
+            ORDER BY prd_start_dt DESC
+        ) AS rn_latest,
+        LEAD(prd_start_dt) OVER (
+            PARTITION BY prd_key_clean
+            ORDER BY prd_start_dt
+        ) AS next_start_dt
+    FROM prd
+)
+INSERT INTO silver.crm_prd_info (
+    prd_id,
+    cat_id,
+    prd_key,
+    prd_nm,
+    prd_cost,
+    prd_line,
+    prd_start_dt,
+    prd_end_dt
+)
+SELECT
+    prd_id,
+    cat_id,
+    prd_key_clean AS prd_key,
+    prd_nm,
+    prd_cost,
+    prd_line,
+    prd_start_dt,
+    CASE 
+        WHEN rn_latest = 1 THEN NULL
+        ELSE DATEADD(DAY, -1, next_start_dt)
+    END AS prd_end_dt
+FROM prd_versions;
+
 
 
         -- =====================================================
